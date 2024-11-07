7330637e9905ded5bd1d2d8565640290a3ca2622
ivysonin <ivysonthauan4@mail.com>
2024-11-07 19:25:00 -0300

    Imprime o maior número

---
+++ funcao05.py
@@ -0,0 +1,8 @@
+def maior_numero () :
+    maior = max (num1,num2)
+    return maior
+
+num1 = int(input ("Digite um número inteiro: "))
+num2 = int(input ("Digite outro número inteiro: "))
+
+print (f"O Maior número entre eles é {maior_numero()}")
