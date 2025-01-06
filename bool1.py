69840ed2e175466036a87dab1230565e4e35461b
ivysonin <ivysonthauan4@mail.com>
2025-01-06 17:43:13 -0300

    Analisando comparações dos valores

---
+++ bool1.py
@@ -0,0 +1,13 @@
+# Crie uma variável x e atribua um valor inteiro a ela. 
+# Use operadores de comparação para verificar se x é:
+# maior que 10, 
+# menor que 5, 
+# igual a 7 e diferente de 3.
+# Imprima os resultados dessas comparações.
+
+x = 10
+
+print(x > 10)
+print(x < 5)
+print(x == 7)
+print(x != 3 )
