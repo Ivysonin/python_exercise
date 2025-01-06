fe9e2bb3dd7d1211bbef7b2b8ecb6b34b81be63d
ivysonin <ivysonthauan4@mail.com>
2025-01-06 17:53:51 -0300

    Comparando True e False com lógica

---
+++ bool2.py
@@ -0,0 +1,11 @@
+# Crie duas variáveis, a e b, com valores booleanos (True ou False).
+# Escreva expressões que usem os operadores lógicos and, or e not 
+# para combinar essas variáveis e imprimir os resultados.
+
+a = True
+b = False
+
+print(a and b) # False: pq precisa que as duas sejam verdadeiras
+print(a or b) # True: pq só precisa que somente uma seja verdadeira
+print(not a) # É True e se transforma em False
+print(not b) # É False e se transforma em True
