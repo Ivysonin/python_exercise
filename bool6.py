b71f372b7800aee90d1242a09673d72bbefdb7bf
ivysonin <ivysonthauan4@mail.com>
2025-01-06 18:34:05 -0300

    verificando se um número é positivo, negativo ou zero utilizando ternário

--- bool6.py
+++ bool6.py
@@ -1,10 +1,6 @@
 # Use a expressão condicional (operador ternário: ) para verificar se um número n é positivo, negativo ou zero.
 # Retorne uma string com o resultado ("Positivo", "Negativo", "Zero").
 
-# formula:
-# valor_se_verdadeiro if condicao else valor_se_falso
-
-
 n = -1
 
 resultado = 'Positivo' if n > 0 else 'Negativo' if n < 0 else 'zero'
