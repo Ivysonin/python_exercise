b4efb5d7085905093fdeab6acf76da7fc2b64a02
ivysonin <ivysonthauan4@mail.com>
2025-01-22 16:01:57 -0300

    Usando input e função split em uma Lista

---
+++ Listas_6.py
@@ -0,0 +1,9 @@
+# Listas
+    # Armazenar mais de uma informação em variáveis
+    # Manter a sequencia dos dados em uma variável
+
+frutas_usuario = input('Digite o nome das frutas separados por vírgulas: ')
+
+lista_frutas = frutas_usuario.split(', ')
+
+print(lista_frutas)
