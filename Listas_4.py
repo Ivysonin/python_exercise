3269e2336e2a83970011017558c321a9b1ca1819
ivysonin <ivysonthauan4@mail.com>
2025-01-22 11:13:01 -0300

    Extraindo variáveis de listas

---
+++ Listas_4.py
@@ -0,0 +1,10 @@
+# Lista de produtos
+produtos = ['arroz', 'feijão', 'carne', 'macarrão', 'cenoura', 'batata', 'tomate']
+
+# Atribuindo variavel a alguns produtos e guardando todos os 'outros'
+produto1, produto2, produto3, *outros = produtos
+
+print(produto1)
+print(produto2)
+print(produto3)
+print(outros)
