7bc5a3b7b92a355f336d44b8f47a1fa04751bdae
ivysonin <ivysonthauan4@mail.com>
2025-01-22 15:52:41 -0300

    Agregando Duas listas com o Zip

---
+++ Listas_5.py
@@ -0,0 +1,10 @@
+# listas
+    # Armazenar mais de uma informação em variáveis
+    # Manter a sequencia dos dados em uma variável
+
+cores = ['amarelo', 'vermelho', 'verde', 'azul']
+valor = [10, 20, 30, 40]
+
+duas_listas = list(zip(cores, valor))
+
+print(duas_listas)
