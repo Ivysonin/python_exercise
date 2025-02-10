9a565a864137c4396ba0e2e7196d75d0e3f58445
ivysonin <ivysonthauan4@gmail.com>
2025-02-10 16:56:09 -0300

    Conhecendo a função lambda e como usar ela (lambda)

---
+++ lambda1.py
@@ -0,0 +1,10 @@
+# Lambda Function
+    # É uma função (pequena) sem nome
+    # Pode ter vários argumentos mas somente 1 expressãao
+    # Código mais 'clean'
+
+soma = lambda x,y: x + y
+subtrair = lambda x,y: x - y
+
+print(soma(2,5))
+print(subtrair(5,2))
