007bc0981ceb8e3e80eb307f6a50626b791c9757
ivysonin <ivysonthauan4@gmail.com>
2025-04-08 21:44:17 -0300

    Tabuada do 1 até 100

---
+++ tabuada1_a_100.py
@@ -0,0 +1,9 @@
+# Todas as tabuadas: 1 até 100
+
+multiplicando = 1
+
+while multiplicando <= 100:
+    for i in range(1, 11):
+        print(f'{multiplicando} x {i} = {multiplicando*i}')
+    multiplicando += 1
+    print('')
